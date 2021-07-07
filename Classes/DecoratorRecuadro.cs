06c3431f7a7c75261946bd22ffe3a0bc1a9b18c4
gabrieleal <gabriel.leal.00@hotmail.com>
2021-07-07 13:02:38 -0300

    se agrega archivo que quedó pendiente

---
+++ Classes/DecoratorRecuadro.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: Usuario
+ * Fecha: 27/06/2021
+ * Hora: 07:29 p.m.
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+using proyectoMetodologiasProgramacion1.Interface;
+
+namespace proyectoMetodologiasProgramacion1.Classes
+{
+	/// <summary>
+	/// Description of decoratorRecuadro.
+	/// </summary>
+	public class DecoratorRecuadro: DecoratorAbstract
+	{
+		public DecoratorRecuadro(ICalificacionDecorator _core)
+		{
+			this.setCore(_core);
+		}
+		
+		public override string mostrarCalificacion()
+		{
+			string cal="*******************************************\n**";
+			cal+=this.getCore().mostrarCalificacion();
+			cal+="**\n*******************************************";
+			
+			return cal;
+		}
+	}
+}
